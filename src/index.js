342a4e5cae86aa3dcaba7ca55c040c8a2bb8f541
Badr BenSlama <badrbenslama8@gmail.com>
2023-09-01 21:38:36 +0100

    linters err fixed

--- src/index.js
+++ src/index.js
@@ -6,19 +6,16 @@ import gameApiSend from './modules/sendScore.js';
 getScores();
 const submit = document.getElementById('submitbtn');
 
-let user = "";
-let score = "";
+let user = '';
+let score = '';
 
 submit.addEventListener('click', () => {
-     user = document.getElementById('player').value;
-     score = document.getElementById('score').value;
-    gameApiSend(user, score);
+  user = document.getElementById('player').value;
+  score = document.getElementById('score').value;
+  gameApiSend(user, score);
 });
 
 const downScores = document.getElementById('refreshBtn');
-downScores.addEventListener("click", () => {
-    getScores();
+downScores.addEventListener('click', () => {
+  getScores();
 });
-
-
-
--- src/modules/sendScore.js
+++ src/modules/sendScore.js
@@ -1,21 +1,19 @@
-const gameApiSend = async (user, score) => { 
+const gameApiSend = async (user, score) => {
   try {
-  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/4clKnBGqzg8jtb9gbQYY/scores', {
-    method: 'POST',
-    body: JSON.stringify({
-      user: user,
-      score: score
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
+    await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/4clKnBGqzg8jtb9gbQYY/scores', {
+      method: 'POST',
+      body: JSON.stringify({
+        user,
+        score,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
 
-  return 'score sent!';
-}
-catch (err)
-{
-  return `an error has occured while sending score! ${err}`;
-}
-}
+    return 'score sent!';
+  } catch (err) {
+    return `an error has occured while sending score! ${err}`;
+  }
+};
 export default gameApiSend;
